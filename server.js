a6914419c33acedfd61695dc15273f07f2861621
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-11-24 13:54:29 -0800

    added error handling and branch support

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ require("dotenv").config()
 const express = require("express")
 const { huntForPokemon } = require("./util")
 
-const { PORT } = process.env
+const { PORT = 3000 } = process.env
 
 const app = express()
 
-app.get("/pokedata/:name", async (req, res) => {
-  const { name } = req.params
+app.get("/pokedata/:branch/:name", async (req, res) => {
+  const { name, branch } = req.params
 
-  const { path } = await huntForPokemon(name);
-  res.redirect(`https://gitlab.com/cable-mc/cobblemon/-/blob/1.4.0/${path}`)
+  try {
+    const { path } = await huntForPokemon(name);
+    res.redirect(`https://gitlab.com/cable-mc/cobblemon/-/blob/${branch}/${path}`)
+  } catch (error) {
+    res.sendStatus(404)
+  }
 })
 
 app.listen(PORT, () => {
